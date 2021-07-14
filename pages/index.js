77b0f36004e17cd42ce71f42eb17e74265aff81d
Darlon Henrique <darlonhenrisouza@gmail.com>
2021-07-14 12:33:03 -0300

    feat(home): profile side bar, welcome area and favorite people area

---
+++ pages/index.js
@@ -0,0 +1,65 @@
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} alt="Profile Photo" />
+    </Box>
+  )
+}
+
+export default function Home() {
+  const githubUser = 'darlonhenrique';
+  const favoritePeople = [
+    'juunegreiros', 'peas',
+    'maykbrito', 'gunnarcorrea',
+    'omariosouto', 'rafaballerini',
+    'filipedeschamps'
+  ]
+
+  return (
+    <>
+      <AlurakutMenu />
+
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem Vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({favoritePeople.length})
+            </h2>
+
+            <ul>
+              {favoritePeople.map((item) => {
+                return (
+                  <li key={item}>
+                    <a href={`/users/${item}`}>
+                      <img src={`https://github.com/${item}.png`} />
+                      <span>{item}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
+}
